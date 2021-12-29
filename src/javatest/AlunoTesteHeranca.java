4221abde8c35656cb01a94924486823132facd92
Ernandes <ernandes77@hotmail.com>
2021-12-29 13:54:11 -0300

    adicionando
    adicionando escrita

--- src/javatest/AlunoTesteHeranca.java
+++ src/javatest/AlunoTesteHeranca.java
@@ -2,6 +2,7 @@ package javatest;
 
 import java1.java.Aluno;
 import java1.java.Diretor;
+import java1.java.Pessoa;
 import java1.java.Secretario;
 
 public class AlunoTesteHeranca {
@@ -15,10 +16,12 @@ public class AlunoTesteHeranca {
 		Diretor diretor = new Diretor();
 		diretor.setTempoDirecao(5);
 		diretor.setRegistroEducacao("123456");
+		diretor.setNome("caio");
 		
 		Secretario secretario = new Secretario();
 		secretario.setExperiencia(1);;
 		secretario.setNivelCargo("secretario");
+		secretario.setNome("Fernanda");
 		
 		System.out.println(aluno);
 		System.out.println(secretario);
@@ -27,6 +30,15 @@ public class AlunoTesteHeranca {
 		System.out.println("Salario Aluno e  = "+aluno.salario());
 		System.out.println("Salario Diretor e = "+diretor.salario());
 		System.out.println("Salario Secretario e = "+secretario.salario());
+		System.out.println("-------------------------------------------------------");
+	
+		teste(secretario);
+		teste(secretario);
+		teste(aluno);
+	
 	}
+	
+	public static void teste(Pessoa pessoa) {
+	System.out.println("essa pessoa e demais " +pessoa.getNome()+ " salario e de  = " +pessoa.salario());}
 
 }
