3bda3f9672d24bdd5b6cee7675f57ae103ac02bc
Ernandes <ernandes77@hotmail.com>
2022-02-02 12:27:59 -0300

    array e vetores

---
+++ src/javatest/ArrayVetores.java
@@ -0,0 +1,20 @@
+package javatest;
+
+public class ArrayVetores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double []notas = new double[4];/*ARRAY SEMPRE DEVE E QUANTIDADE DE POSICAO DEFENIDAS*/
+
+		notas[0] = 9.8;
+		notas[1] = 10.0;
+		notas[2]= 5.9;
+		notas[3] = 3.5;
+		for(int pos = 0;pos <=3;pos++ ) {
+			
+		
+		
+		System.out.println("nota = "+(pos+1)+" notas "+notas[pos]);
+	}}
+
+}
