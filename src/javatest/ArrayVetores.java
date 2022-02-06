1888eda7f89b0f01dae9072376011d15028f370b
Ernandes <ernandes77@hotmail.com>
2022-02-06 11:54:51 -0300

    ADICIONANDO OBJETO E NOTAS COM ARRAY

--- src/javatest/ArrayVetores.java
+++ src/javatest/ArrayVetores.java
@@ -2,26 +2,32 @@ package javatest;
 
 import javax.swing.JOptionPane;
 
+import java1.java.Aluno;
+import java1.java.Disciplina;
+
 public class ArrayVetores {
 
 	public static void main(String[] args) {
 	
-		String posicoes = JOptionPane.showInputDialog("qual a poisicao do array: ");
-		
-		double[] notas = new double[Integer.parseInt(posicoes)];
-		
-	for(int pos = 0; pos <notas.length;pos++) {
-		String valor = JOptionPane.showInputDialog("entre com a nota: "+pos);
-		notas[pos] = Double.valueOf(valor);
-		
-	}
-		
-		for(int pos = 0; pos < notas.length;pos++) {
-			
-		
-		System.out.println("nota "+ (pos  +1)+ " e = " +notas[pos]);
-		}
-	}	
+	Aluno aluno = new Aluno();
+	aluno.setNome("ernandes");
+	aluno.setIdade(33);
 	
+	Disciplina disciplina = new Disciplina();
+	disciplina.setDisciplina("java");
+	double[] notas = {2.3,3.2,6.5,4.5};
+	
+	disciplina.setNota(notas);
+	
+	aluno.getDisciplinas().add(disciplina);
+	
+	Disciplina disciplina2 = new Disciplina();
+	disciplina2.setDisciplina("css");
+	double[] notasCss = {9.9,8.4,2.5,3.5};
+	
+	disciplina2.setNota(notasCss);
+	
+	
+	}
 
 }
