4a820a32bf5e2c9c12e481623c4de40a7c74e816
Ernandes <ernandes77@hotmail.com>
2022-02-25 10:31:37 -0300

    processamento paralelo

--- src/threds/AulaThred.java
+++ src/threds/AulaThred.java
@@ -1,16 +1,35 @@
 package threds;
 
+import javax.swing.JOptionPane;
+
 public class AulaThred {
 
 	public static void main(String[] args) throws InterruptedException {
 		 
-		for(int pos = 0; pos <=10; pos++) {
-			
-			System.out.println("Executando algo por exemplo. envio de email ");
-			
-			Thread.sleep(1000);
-		}
-
+		new Thread() {/*Inicia uma thread*/
+			public void run() {/*run executa o que nos quisemos*/
+				/*codigo da rotina*/
+				
+				/*codigo da rotina executando*/
+				for(int pos = 0; pos <=10; pos++) {
+					
+					System.out.println("Executando algo por exemplo. envio de email ");
+					
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}/*Da um tempo no envio */
+				
+				}
+			};
+		}.start();/*liga a thread que fica processando paralera por tras*/
+		
+		
+		System.out.println("Chegou ao fim do teste de thread !!!");
+		
+		JOptionPane.showMessageDialog(null, "Sistema continua executando para usuario!!!");
 	}
 
 }
