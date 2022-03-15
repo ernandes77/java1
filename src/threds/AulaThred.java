a71f8aa6a129426784ffa6a9802530d4cc27f615
Ernandes <ernandes77@hotmail.com>
2022-03-15 10:48:30 -0300

    thread

--- src/threds/AulaThred.java
+++ src/threds/AulaThred.java
@@ -6,49 +6,56 @@ public class AulaThred {
 
 	public static void main(String[] args) throws InterruptedException {
 		 
-		new Thread() {/*Inicia uma thread*/
-			public void run() {/*run executa o que nos quisemos*/
-				/*codigo da rotina*/
-				
-				/*codigo da rotina executando*/
-				for(int pos = 0; pos <=10; pos++) {
-					
-					System.out.println("Executando algo por exemplo. envio de email ");
-					
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}/*Da um tempo no envio */
-				
-				}
-			};
-		}.start();/*liga a thread que fica processando paralera por tras*/
+		Thread threademail =new Thread(thread1);
+		threademail.start();
 		
 		/*----------Divisao das threads------*/
 		
-		new Thread() {
-			public void run() {
-				for(int pos = 0; pos <=10;pos++) {
-			
-					System.out.println("envio de nota fiscal!!!");
-					
-					try {
-						Thread.sleep(4000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			};
-			
-		}.start();
-		
+		Thread threadfiscal = new Thread(thread2); 
+		threadfiscal.start();
 		
 		System.out.println("Chegou ao fim do teste de thread !!!");
 		
 		JOptionPane.showMessageDialog(null, "Sistema continua executando para usuario!!!");
 	}
 
+	private static Runnable thread2 = new Runnable() {
+		
+		@Override
+		public void run() {
+			for(int pos = 0; pos <=10;pos++) {//inicio do codigo paralelo
+				
+				System.out.println("envio de nota fiscal!!!");//fim do codigo parelelo
+				
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	};
+	
+	
+	private static Runnable thread1 = new Runnable() {
+		public void run() {
+			
+			/*codigo da rotina executando*/
+			for(int pos = 0; pos <=10; pos++) {
+				
+				System.out.println("Executando algo por exemplo. envio de email ");
+				
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}/*Da um tempo no envio */
+			
+			}
+			
+		}
+	};
 }
