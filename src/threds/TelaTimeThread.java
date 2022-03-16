ff6a74cd3fc05d245d4e653831ccdf56aa0f9f34
Ernandes <ernandes77@hotmail.com>
2022-03-16 14:04:21 -0300

    jdialog

--- src/threds/TelaTimeThread.java
+++ src/threds/TelaTimeThread.java
@@ -1,16 +1,21 @@
 package threds;
 
 import java.awt.Dimension;
-
+import java.awt.GridBagConstraints;
 import javax.swing.JDialog;
 
 public class TelaTimeThread extends JDialog{
 	
 	public TelaTimeThread() {//executar oque tiver dentro na hora 
-		setTitle("Minha tela");
-		setSize(new Dimension (240, 240));
-		setLocationRelativeTo(null);
+		setTitle("Minha tela");//title nome do titulo da tela
+		setSize(new Dimension (240, 240));//tamanho da tela
+		setLocationRelativeTo(null);//centralaiza tela
 		setResizable(false);
+		//primeira etapa concluida
+		
+		
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.Gridx();
 	
 	
 	
